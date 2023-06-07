9a2939127c52dedb942edda970c4a658f29a10bd
lijiaren <R_Richie@163.com>
2023-06-07 16:56:41 +0800

    http调试

--- springcloud-alibaba-example/middleware/gateway-admin/src/main/java/com/R/sae/ga/model/dto/RouteData.java
+++ springcloud-alibaba-example/middleware/gateway-admin/src/main/java/com/R/sae/ga/model/dto/RouteData.java
@@ -3,7 +3,23 @@ package com.R.sae.ga.model.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
 @Data
 @Builder
 public class RouteData {
+
+    private String id;
+
+    private List<PredicateDefinition> predicates;
+
+    private List<FilterDefinition> filters;
+
+    private URI uri;
+
+    private Map<String, Object> metadata;
+
+    private int order = 0;
 }
