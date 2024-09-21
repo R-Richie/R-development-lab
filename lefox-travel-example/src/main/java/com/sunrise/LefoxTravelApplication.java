ac22fbe2cabb929e159a1f61dbe9c3fb3fe546af
lijiaren <lijiaren1990@gmail.com>
2024-09-21 23:04:22 +0800

    初始化项目

---
+++ lefox-travel-example/src/main/java/com/sunrise/LefoxTravelApplication.java
@@ -0,0 +1,11 @@
+package com.sunrise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LefoxTravelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LefoxTravelApplication.class, args);
+    }
+}
